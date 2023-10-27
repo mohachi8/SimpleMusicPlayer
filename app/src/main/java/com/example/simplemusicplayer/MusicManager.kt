cef209fef3afaf26a98dcc2cb124b582603ee316
mohachi8 <92675372+mohachi8@users.noreply.github.com>
2023-10-27 18:18:32 +0900

    連続でスタートボタンを押したとき、重複して音楽が再生されないように修正

--- app/src/main/java/com/example/simplemusicplayer/MusicManager.kt
+++ app/src/main/java/com/example/simplemusicplayer/MusicManager.kt
@@ -8,10 +8,14 @@ class MusicManager(context: Context) {
     private lateinit var mediaPlayer: MediaPlayer
     private val myContext: Context = context
 
-    fun startMusic(){
-        mediaPlayer = MediaPlayer.create(myContext,R.raw.music)
-        mediaPlayer.start()
+    fun startMusic() {
+        // MediaPlayerが初期化されておらず、または再生中でない場合のみ音楽を再生
+        if (!::mediaPlayer.isInitialized || !mediaPlayer.isPlaying) {
+            mediaPlayer = MediaPlayer.create(myContext, R.raw.music)
+            mediaPlayer.start()
+        }
     }
+
 
     fun stopMusic(){
         mediaPlayer.stop()
