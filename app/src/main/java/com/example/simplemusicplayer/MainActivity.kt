be8b4dfd6511e5d7c5000c7a490731efc08ed4ab
mohachi8 <92675372+mohachi8@users.noreply.github.com>
2023-10-27 16:43:19 +0900

    Add music playback functionality
    - Implemented MusicManager class for music controls

--- app/src/main/java/com/example/simplemusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/simplemusicplayer/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.simplemusicplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.simplemusicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var musicManager: MusicManager = MusicManager(this@MainActivity)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // ViewBindingを使用してUI要素を初期化
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // MusicManagerのインスタンスを生成
+        musicManager = MusicManager(this)
+
+        // Startボタンのクリックリスナー
+        binding.startButton.setOnClickListener {
+            musicManager.startMusic()
+        }
+
+        // Stopボタンのクリックリスナー
+        binding.stopButton.setOnClickListener {
+            musicManager.stopMusic()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Activityが破棄される時に音楽のリソースも解放
+        musicManager.stopMusic()
     }
 }
--- app/src/main/java/com/example/simplemusicplayer/MusicManager.kt
+++ app/src/main/java/com/example/simplemusicplayer/MusicManager.kt
@@ -1,4 +1,21 @@
 package com.example.simplemusicplayer
 
-class MusicManager {
+import android.content.Context
+import android.media.MediaPlayer
+
+class MusicManager(context: Context) {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private val myContext: Context = context
+
+    fun startMusic(){
+        mediaPlayer = MediaPlayer.create(myContext,R.raw.music)
+        mediaPlayer.start()
+    }
+
+    fun stopMusic(){
+        mediaPlayer.stop()
+        mediaPlayer.reset()
+        mediaPlayer.release()
+    }
 }
